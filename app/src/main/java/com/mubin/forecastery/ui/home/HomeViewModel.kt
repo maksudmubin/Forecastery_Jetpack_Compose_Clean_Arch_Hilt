64d5fd73201de340e0f179e3be91025e6002100f
Mubin <maksudmubin@gmail.com>
2024-12-04 17:37:20 +0600

    # Viewmodel updated to update and handle states of the UI, This branch is not complete

--- app/src/main/java/com/mubin/forecastery/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/mubin/forecastery/ui/home/HomeViewModel.kt
@@ -1,5 +1,10 @@
 package com.mubin.forecastery.ui.home
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.mubin.forecastery.base.utils.MsLogger
 import com.mubin.forecastery.base.utils.UseCase
@@ -28,8 +33,41 @@ class HomeViewModel @Inject constructor(
     private val districtListUseCase: GetDistrictListUseCase
 ) : ViewModel() {
 
-    // Lazy initialization of UI state
-    val uiState by lazy { HomeUiState() }
+
+
+    private val _permissionState = mutableStateOf<PermissionState>(PermissionState.Denied)
+    val permissionState = _permissionState
+
+    private val _askPermission = mutableStateOf(false)
+    val askPermission: State<Boolean> = _askPermission
+
+    private val _permissionGranted = mutableStateOf(false)
+    val permissionGranted: State<Boolean> = _permissionGranted
+
+    private val _loadData = mutableStateOf(false)
+    val loadData: State<Boolean> = _loadData
+
+    private val _homeState = mutableStateOf<WeatherState>(WeatherState.Loading)
+    val homeState: State<WeatherState> = _homeState
+
+    private val _searchState = mutableStateOf<SearchState>(SearchState.Loading)
+    val searchState: State<SearchState> = _searchState
+
+    private var _currentLocation: WeatherRequest? by mutableStateOf(null)
+    var currentLocation: WeatherRequest?
+        get() = _currentLocation
+        set(value) {
+            _currentLocation = value
+        }
+
+    private var _weatherRequest: WeatherRequest? by mutableStateOf(null)
+    var weatherRequest: WeatherRequest?
+        get() = _weatherRequest
+        set(value) {
+            _weatherRequest = value
+        }
+
+    var cachedDistrictList = mutableStateListOf<DistrictModel>()
 
     /**
      * Fetches weather details based on the provided request parameters.
@@ -39,7 +77,7 @@ class HomeViewModel @Inject constructor(
      *
      * This function is executed in the `IO` context for offloading heavy tasks from the main thread.
      */
-    suspend fun getWeatherDetails(request: WeatherRequest): WeatherEntity? = withContext(Dispatchers.IO) {
+    suspend fun getWeatherDetails(request: WeatherRequest): Result<WeatherEntity> = withContext(Dispatchers.IO) {
         // Log the initiation of the API call
         MsLogger.d("HomeViewModel", "Fetching weather details with request: $request")
 
@@ -51,7 +89,7 @@ class HomeViewModel @Inject constructor(
         } catch (e: Exception) {
             // Log any exceptions that occur
             MsLogger.d("HomeViewModel", "Error fetching weather data: ${e.message}")
-            null
+            Result.failure(e)
         }
     }
 
@@ -63,7 +101,7 @@ class HomeViewModel @Inject constructor(
      *
      * @return A list of `DistrictModel` objects if successful, or `null` in case of an error.
      */
-    suspend fun getDistrictList(): List<DistrictModel>? = withContext(Dispatchers.IO) {
+    suspend fun getDistrictList(): Result<List<DistrictModel>> = withContext(Dispatchers.IO) {
         try {
             // Log the initiation of the district list fetch operation
             MsLogger.d("HomeViewModel", "Fetching district list using use case.")
@@ -71,14 +109,61 @@ class HomeViewModel @Inject constructor(
             // Execute the use case to retrieve the district list
             districtListUseCase(UseCase.None()).also {
                 // Log the successful retrieval of data
-                MsLogger.d("HomeViewModel", "Successfully fetched ${it?.size ?: 0} districts.")
+                MsLogger.d("HomeViewModel", "Successfully fetched ${it.getOrNull()?.size} districts.")
             }
         } catch (e: Exception) {
             // Log the exception if an error occurs
             MsLogger.e("HomeViewModel", "Error fetching district list: ${e.message}")
-
             // Return null to indicate failure
-            null
+            Result.failure(e)
         }
     }
+
+    fun requestPermission() {
+        permissionState.value = PermissionState.Requesting
+    }
+
+    fun setAskPermissionState(askPermission: Boolean) {
+        _askPermission.value = askPermission
+    }
+
+    fun setPermissionGrantedState(permissionGranted: Boolean) {
+        _permissionGranted.value = permissionGranted
+    }
+
+    fun setLoadDataState(loadData: Boolean) {
+        _loadData.value = loadData
+    }
+
+    // Set loading state
+    fun setHomeLoadingState() {
+        _homeState.value = WeatherState.Loading
+    }
+
+    // Set success state with weather data
+    fun setHomeSuccessState(data: WeatherEntity) {
+        _homeState.value = WeatherState.Success(data)
+    }
+
+    // Set error state with message
+    fun setHomeErrorState(message: String?) {
+        _homeState.value = WeatherState.Error(message)
+    }
+
+    // Set loading state
+    fun setSearchLoadingState() {
+        _searchState.value = SearchState.Loading
+    }
+
+    // Set success state with weather data
+    fun setSearchSuccessState(data: List<DistrictModel>) {
+        _searchState.value = SearchState.Success(data)
+        cachedDistrictList.addAll(data)
+    }
+
+    // Set error state with message
+    fun setSearchErrorState(message: String?) {
+        _searchState.value = SearchState.Error(message)
+    }
+
 }
